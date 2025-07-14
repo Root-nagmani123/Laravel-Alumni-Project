c17a85fc6623d2a00414acceb68774eeeeb93484
dhananjaysingh <dhananjaysingh.negd@digitalindia.gov.in>
2025-07-14 16:26:42 +0530

    cleaned code

--- app/Http/Controllers/User/ProfileController.php
+++ app/Http/Controllers/User/ProfileController.php
@@ -26,157 +26,19 @@ class ProfileController extends Controller
     $user = auth()->guard('user')->user();
     $userId = $user->id;
 
-    // $profile_topic = Post::with(['media', 'member'])
-    //     ->whereHas('member', function ($query) use ($userId) {
-    //         $query->where('id', $userId);
-    //     })
-    //     ->get();
-
-        $posts = Post::with(['member', 'media', 'likes', 'comments.member'])
+         $posts = Post::with(['member', 'media', 'likes', 'comments.member'])
         ->orderBy('created_at', 'desc')
         ->where('id', $userId)
         ->get();
-        //echo '<pre>';print_r($posts); die;
-
 
     return view('profile', compact('user','posts'));
 }
-
-
-   /* public function edit(Request $request): View
-    {
-        return view('edit', [
-            'user' => $request->user(),
-        ]);
-    }*/
-
-
-    public function edit($id)
+  public function edit($id)
         {
             $member = Member::findOrFail($id);
             return view('profile', compact('member'));
         }
 
-    /**
-     * Update the user's profile information.
-     */
-    /*public function update(ProfileUpdateRequest $request): RedirectResponse
-    {
-        $request->user()->fill($request->validated());
-
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
-        }
-
-        $request->user()->save();
-
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
-    }*/
-
-    /*
-    public function update(Request $request, $id): JsonResponse
-        {
-            echo $id;
-            dd($request);
-            exit;
-
-            $request->validate([
-                'name' => 'required|string|max:255',
-                'date_of_birth' => 'required|date',
-                'place_of_birth' => 'required|string',
-                'gender' => 'required|in:male,female,other',
-                'mobile' => 'required|digits:10',
-                'address' => 'required|string',
-                'bio' => 'required|string',
-            ]);
-
-
-        if ($request->hasFile('profile_pic')) {
-        $file = $request->file('profile_pic');
-        $filename = time() . '_' . $file->getClientOriginalName();
-        $file->move(public_path('assets/uploads/profile_pic'), $filename);
-        $user->profile_pic = $filename;
-    }
-
-
-            $user = User::findOrFail($id);
-            $user->fill($request->all());
-
-
-            $user->save();
-
-            return response()->json(['status' => 'Profile updated successfully']);
-        }
-*/
-
-/*public function update(Request $request, $id): RedirectResponse
-{
-       $request->validate([
-        'name' => 'required|string|max:255',
-        'date_of_birth' => 'required|date',
-        'place_of_birth' => 'required|string',
-        'gender' => 'required|in:male,female,other',
-        'mobile' => 'required|digits:10',
-        'address' => 'required|string',
-        'bio' => 'required|string',
-        'marital_status' => 'required|in:single,married,divorced',
-
-    ]);
-
-    $user = member::findOrFail($id);
-
-     $user->fill($request->except(['profile_pic']));
-
-    // profile picture upload
-    if ($request->hasFile('profile_pic')) {
-        $file = $request->file('profile_pic');
-        $filename = time() . '_' . $file->getClientOriginalName();
-        $file->move(public_path('assets/uploads/profile_pic'), $filename);
-        $user->profile_pic = $filename;
-    }
-
-    $user->save();
-
-    return redirect()->back()->with('success', 'Profile updated successfully.');
-    return redirect()->back()->with('active_tab', 'personal');
-}
-        */
-
-     /*   public function update(Request $request, $id): RedirectResponse
-{
-    $request->validate([
-        'name' => 'required|string|max:255',
-        'date_of_birth' => 'required|date',
-        'place_of_birth' => 'required|string',
-        'gender' => 'required|in:male,female,other',
-        'mobile' => 'required|digits:10',
-        'address' => 'required|string',
-        'bio' => 'required|string',
-        'marital_status' => 'required|in:single,married,divorced',
-    ]);
-
-    $user = member::findOrFail($id);
-
-    $user->fill($request->except(['profile_pic']));
-
-    // Handle profile picture upload
-    if ($request->hasFile('profile_pic')) {
-        $file = $request->file('profile_pic');
-        $filename = time() . '_' . $file->getClientOriginalName();
-        $file->move(public_path('assets/uploads/profile_pic'), $filename);
-
-        $user->profile_pic = $filename;
-    }
-
-    $user->save();
-     session(['theme' => 'dark']); // or 'light'
-    return redirect()->back()->with([
-        'success' => 'Profile updated successfully.',
-        'active_tab' => 'personal'
-    ]);
-
-}
-    */
 
     public function update(Request $request, $id): RedirectResponse
 {
