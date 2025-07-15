2d8bd88f868f0b644bf7bac4a394965309e8e586
dhananjaysingh <dhananjaysingh.negd@digitalindia.gov.in>
2025-07-15 16:09:12 +0530

    load more messages just now issue fixed

--- resources/views/partials/user_feed.blade.php
+++ resources/views/partials/user_feed.blade.php
@@ -694,7 +694,7 @@ document.addEventListener('DOMContentLoaded', function () {
                                         <div class="bg-light rounded-start-top-0 p-3 rounded">
                                             <div class="d-flex justify-content-between">
                                                 <h6 class="mb-1"><a href="#!">${comment.member?.name || 'Anonymous'}</a></h6>
-                                                <small class="ms-2">Just now</small>
+                                                <small class="ms-2">{{ $comment->created_at->diffForHumans() }}</small>
                                             </div>
                                             <p class="small mb-0">${comment.comment}</p>
                                         </div>
