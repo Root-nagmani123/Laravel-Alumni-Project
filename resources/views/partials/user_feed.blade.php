bdef943f990f717e13d492a42479ddb7b05dc96b
vinita <vinita.gupta@awzpact.com>
2025-07-29 12:56:14 +0530

    commentcode

--- resources/views/partials/user_feed.blade.php
+++ resources/views/partials/user_feed.blade.php
@@ -332,7 +332,7 @@
                                 <div class="d-flex justify-content-between">
                                     <h6 class="mb-1"> <a href="#!"> {{ $comment->member->name ?? 'Anonymous' }} </a>
                                     </h6>
-                                    <small class="ms-2">${comment.created_at_human || comment.created_at || ''}</small>
+                                    <small class="ms-2">{{ $comment->created_at->diffForHumans() }}</small>
                                 </div>
                                 <p class="small mb-0" id="comment-text-{{ $comment->id }}">{{ $comment->comment }}</p>
                             </div>
