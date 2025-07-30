f189e25f9391750d00b0b8e3d6d835eae3218b71
Root-nagmani123 <nagmani.singh@awzpact.com>
2025-07-30 15:34:06 +0530

    login remove

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -8,16 +8,16 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
-
 class AuthenticatedSessionController extends Controller
 {
     /**
      * Display the login view.
      */
-    public function create(): View
-    {
-        return view('auth.login');
-    }
+ public function create(): RedirectResponse
+{
+    return redirect('/user/login');
+}
+
 
     /**
      * Handle an incoming authentication request.
