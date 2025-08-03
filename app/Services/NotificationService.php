4ba1d26617617fb3d83ceb9a50c5606a220ddc25
vinita <vinita.gupta@awzpact.com>
2025-08-03 18:26:11 +0530

    all notification

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -4,6 +4,8 @@ namespace App\Services;
 
 use App\Models\Notification;
 use App\Models\Member;
+use App\Models\GroupMember; // Added this import
+use Carbon\Carbon; // Added this import
 
 class NotificationService
 {
@@ -37,8 +39,9 @@ class NotificationService
 
 
         //topic added to group or forum
-        public function notifyGroupOrForumMembers(array $memberIds, string $type, string $message, $sourceId, string $sourceType, $fromUserId = null)
+        public function notifyGroupOrForumMembers( $memberIds, $type, $message, $sourceId,  $sourceType, $fromUserId = null)
         {
+            
             return Notification::create([
                 'from_user_id' => $fromUserId,
                 'type'         => $type, // e.g., 'group_add', 'forum_topic'
@@ -66,13 +69,14 @@ class NotificationService
 //like or comment on post
     public function notifyPostOwner(int $postOwnerId, int $fromUserId, string $type, string $message, $sourceId, string $sourceType)
     {
+
         return $this->createNotification([
             'from_user_id' => $fromUserId,
             'type'         => $type, // 'like' or 'comment'
             'message'      => $message,
             'source_id'    => $sourceId,
             'source_type'  => $sourceType,
-            'user_id'     => [$postOwnerId],
+            'user_id'     => json_encode([$postOwnerId]),
         ]);
     }
 
