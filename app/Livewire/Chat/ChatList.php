707a13560878afe6c29fc023c62a40419de47814
Mukesh Yadav <mukesh.yadav@awzpact.com>
2025-08-05 15:48:58 +0530

    - changes

--- app/Livewire/Chat/ChatList.php
+++ app/Livewire/Chat/ChatList.php
@@ -4,88 +4,81 @@ namespace App\Livewire\Chat;
 
 use Livewire\Component;
 use App\Models\Member;
+use App\Models\Message;
+use App\Events\MessageSentEvent;
+use Livewire\Attributes\On;
+use Illuminate\Support\Facades\Auth;
 
 class ChatList extends Component
 {
     public $search = '';
-    public $openChats = [];
-    public $newMessage = [];
-    public $messages = [];
     
+    public $newMessage = '';
+    public $messages = [];
+    public $selectedChat = null;
+    public $senderId = auth()->guard('user')->id();
+
     public function selectChat($chatId)
     {
-        if (!in_array($chatId, $this->openChats)) {
-            $this->openChats[] = $chatId;
+        $this->selectedChat = $chatId;
+        $this->loadMessages();
+    }
+
+    public function loadMessages()
+    {
+        if ($this->selectedChat) {
+            $this->messages = Message::where(function ($query) {
+                $query->where('sender_id', auth()->guard('user')->id())
+                    ->where('receiver_id', $this->selectedChat);
+            })
+                ->orWhere(function ($query) {
+                    $query->where('sender_id', $this->selectedChat)
+                        ->where('receiver_id', auth()->guard('user')->id());
+                })
+                ->orderBy('created_at', 'asc')
+                ->get();
         }
     }
 
     public function closeChat($chatId)
     {
-        $this->openChats = array_filter($this->openChats, fn ($id) => $id != $chatId);
+        $this->reset(['selectedChat', 'newMessage']);
     }
 
-    public function submit() 
+    public function submit()
     {
         if (empty($this->newMessage)) {
             return;
         }
 
-        // Store In DB Logic
-        // ChatMessage::create([
-        //     'member_id' => auth()->id(),
-        //     'message' => $this->newMessage,
-        // ]);
-        $this->messages[] = [
-            'message' => $this->newMessage,
-        ];
-        
-        \Log::info('New message submitted: ' . $this->newMessage);
-        
-        broadcast(new \App\Events\MessageSent($this->newMessage));
-        $this->newMessage = '';
+        $message = new Message();
+        $message->sender_id = auth()->guard('user')->id();
+        $message->receiver_id = $this->selectedChat;
+        $message->message = $this->newMessage;
+        $message->save();
+
+        // broadcast(new MessageSentEvent($message))->toOthers();
+        broadcast(new MessageSentEvent($message))->toOthers();
+        $this->reset(['newMessage']);
     }
 
-    function getListeners()
-    {
-        return [
-            'echo-private:channel-name,MessageSent' => 'newChatMessageNotification',
-        ];
-    }
-
-    function newChatMessageNotification($event)
-    {
-        \Log::info('New chat message notification received: ' . json_encode($event));
-        $this->messages[] = [
-            'id' => $event['id'],
-            'sender_id' => $event['sender_id'],
-            'receiver_id' => $event['receiver_id'],
-            'message' => $event['message'],
-        ];
-        
-        // Optionally, you can also update the openChats or perform other actions
-        if (!in_array($event['sender_id'], $this->openChats)) {
-            $this->openChats[] = $event['sender_id'];
-        }
-    }
+    
 
     public function render()
     {
         $chats = Member::query();
 
-        if($this->search) {
+        if ($this->search) {
             $chats = $chats->where('name', 'like', '%' . $this->search . '%');
         }
 
         $chats = $chats->limit(10)->get();
-
-        $openMembers = Member::whereIn('id', $this->openChats)->get();
-
-        // $this->messages = [];
-
+        // dump($chats);
+        $selectChat = $this->selectedChat ? Member::find($this->selectedChat) : null;
 
         return view('livewire.chat.chat-list', [
             'chats' => $chats,
-            'openMembers' => $openMembers,
+            'selectChat' => $selectChat,
             'messages' => $this->messages,
         ]);
     }
