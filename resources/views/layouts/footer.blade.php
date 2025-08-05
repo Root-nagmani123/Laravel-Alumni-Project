00d96b1d544e4f425c0cd9db3bee5901c5d3c5e4
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2025-08-05 14:09:31 +0530

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -138,8 +138,7 @@ JS libraries, plugins and custom scripts -->
 
 <!-- Select2 JS -->
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-<!-- Bootstrap 5 Bundle (includes Popper) -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
 
 <script>
     $(document).ready(function () {
