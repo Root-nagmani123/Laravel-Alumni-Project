6cfafcdbdc5bf270eb1945964c59c971b5350dab
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2025-08-11 13:00:09 +0530

    comment image

--- resources/views/partials/right-sidebar.blade.php
+++ resources/views/partials/right-sidebar.blade.php
@@ -10,7 +10,7 @@
             </div>
             <!-- Card header END -->
             <!-- Card body START -->
-            <div class="card-body  overflow-auto" style="max-height: 500px;">
+            <div class="card-body">
 
                 <!-- News item -->
                 <!-- Links -->
@@ -21,6 +21,20 @@
         </div>
     </div>
 
+        <div class="col-sm-6 col-lg-12">
+        <div class="card">
+            <!-- Card header START -->
+            <div class="card-header pb-0 border-0">
+                <h5 class="card-title mb-0">Newsletter</h5>
+            </div>
+            <!-- Card header END -->
+            <!-- Card body START -->
+            <div class="card-body">
+                <a href="https://www.lbsnaa.gov.in/lbsnaa-newsletter" class="text-decoration-none" style="color:#af2910;" target="_blank">Newsletter</a>
+            </div>
+            <!-- Card body END -->
+        </div>
+    </div>
     <!-- Card News END -->
     <!-- Card News START -->
     <div class="col-sm-6 col-lg-12">
