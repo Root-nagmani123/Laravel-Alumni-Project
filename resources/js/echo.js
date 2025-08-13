17fd1a30a25b334ec525239a5307c0b23178b6d9
Mukesh Yadav <mukesh.yadav@awzpact.com>
2025-08-13 12:53:29 +0530

    - echo file change

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -7,12 +7,9 @@ window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
-    // wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    // wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    wsPort: 443,
-    wssPort: 443,
-    // forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    forceTLS: true,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
 });
 
