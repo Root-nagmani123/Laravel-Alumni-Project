8af303a434167396922835a099792b78321452d8
Mukesh Yadav <mukesh.yadav@awzpact.com>
2025-08-13 15:10:27 +0530

    - log added

--- routes/channels.php
+++ routes/channels.php
@@ -8,9 +8,11 @@ use Illuminate\Support\Facades\Broadcast;
 
 
 Broadcast::channel('chat-channel.{userId}', function ($user, $userId) {
+    \Log::info('Chat channel accessed by user: ' . $user->id . ' for userId: ' . $userId);
     return (int) $user->id === (int) $userId;
 }, ['guards' => ['user', 'member']]);
 
 Broadcast::channel('unread-channel.{receiverId}', function ($user, $receiverId) {
+    \Log::info('Unread channel accessed by user: ' . $user->id . ' for receiverId: ' . $receiverId);
     return (int) $user->id === (int) $receiverId;
 }, ['guards' => ['user', 'member']]);
