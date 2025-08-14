76ffb0479d95708f133ca91284b1355197a73ffe
Mukesh Yadav <mukesh.yadav@awzpact.com>
2025-08-14 11:30:37 +0530

    - chnages in echo js

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -6,17 +6,19 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
+    // wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsHost: 'alumni.lbsnaa.gov.in',
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    // forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    forceTLS: true,
     enabledTransports: ['ws', 'wss'],
     withCredentials: true,
 
     authorizer: (channel, options) => {
         return {
             authorize: (socketId, callback) => {
-                axios.post('/broadcasting/auth', {
+                axios.post('https://alumni.lbsnaa.gov.in/broadcasting/auth', {
                     socket_id: socketId,
                     channel_name: channel.name
                 }, {
