2b2f68f67f6deaeaed64043bc5cf9ca16d3edf0b
Mukesh Yadav <mukesh.yadav@awzpact.com>
2025-08-26 16:23:54 +0530

    - instead of mentor....mente is apeearing

--- app/Livewire/Chat/ChatList.php
+++ app/Livewire/Chat/ChatList.php
@@ -221,7 +221,7 @@ class ChatList extends Component
         $results = DB::table('mentor_mentee_connection')
         ->join('members as mentors', 'mentor_mentee_connection.mentee_id', '=', 'mentors.id')
         ->where('mentor_mentee_connection.mentor_id', $user_id)
-        ->select('mentors.id as member_id', 'mentors.name as name', 'mentors.cader as cadre', 'mentors.batch', 'mentors.sector', 'mentors.profile_pic', DB::raw("'mentor' as role_type"))
+        ->select('mentors.id as member_id', 'mentors.name as name', 'mentors.cader as cadre', 'mentors.batch', 'mentors.sector', 'mentors.profile_pic', DB::raw("'mentee' as role_type"))
         ->get();
 
         return $results;
@@ -234,7 +234,7 @@ class ChatList extends Component
         $results = DB::table('mentor_mentee_connection')
             ->join('members as mentors', 'mentor_mentee_connection.mentor_id', '=', 'mentors.id')
             ->where('mentor_mentee_connection.mentee_id', $user_id)
-            ->select('mentors.id as member_id', 'mentors.name as name', 'mentors.cader as cadre', 'mentors.batch', 'mentors.sector', 'mentors.profile_pic', DB::raw("'mentee' as role_type"))
+            ->select('mentors.id as member_id', 'mentors.name as name', 'mentors.cader as cadre', 'mentors.batch', 'mentors.sector', 'mentors.profile_pic', DB::raw("'mentor' as role_type"))
             ->get();
 
         return $results;
