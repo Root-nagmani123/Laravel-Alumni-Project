5bfb72ee9af9bc11f29bc5b6a1fb3fa711375495
nibhashlbsnaa <nibhash.mishra@awzpact.com>
2025-08-29 17:01:20 +0530

    group user & admin

--- resources/views/layouts/group-modal-admin.blade.php
+++ resources/views/layouts/group-modal-admin.blade.php
@@ -1,3 +1,18 @@
+@php
+    $years = [];
+    $cadres = [];
+    $years = App\Models\Member::where('service', 'IAS')
+        ->whereNotNull('batch')
+        ->where('batch', '!=', 'NA')
+        ->distinct()
+        ->pluck('batch');
+    $cadres = App\Models\Member::query();
+    $cadres = $cadres->whereNotNull('cader')
+                ->where('cader', '!=', 'NA')
+                ->distinct()
+                ->orderBy('cader')
+                ->pluck('cader');
+@endphp
 {{-- <form action="{{ route('user.group.store') }}" method="POST" enctype="multipart/form-data"> --}}
     <form id="groupForm1">
         @csrf
@@ -40,6 +55,9 @@
                         <label class="form-label">Year / Batch</label>
                         <select class="form-select year-select" name="year[]" multiple="multiple">
                             <!-- Options populated dynamically via AJAX -->
+                            @foreach($years as $year)
+                                <option value="{{ $year }}">{{ $year }}</option>
+                            @endforeach
                         </select>
                     </div>
 
@@ -49,6 +67,9 @@
                         <select class="form-select cadre select2" name="cadre[]" multiple="multiple"
                             data-id="new_group_create">
                             <!-- Options populated dynamically via AJAX -->
+                            @foreach($cadres as $cadre)
+                                <option value="{{ $cadre }}">{{ $cadre }}</option>
+                            @endforeach
                         </select>
                     </div>
 
