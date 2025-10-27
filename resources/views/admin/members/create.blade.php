cb0935491c78db1f00b5f31a9aa9d5cd471b35fd
Root-nagmani123 <nagmani.singh@awzpact.com>
2025-10-27 15:53:29 +0530

    security bug solve

--- resources/views/admin/members/create.blade.php
+++ resources/views/admin/members/create.blade.php
@@ -188,7 +188,7 @@
                             </div>
                             <div class="col-md-6">
                                 <div class="mb-3">
-                                    <label class="form-label">Profile Photo</label>
+                                    <label class="form-label">Profile Photo <span class="required text-danger">*</span></label>
                                     <input type="file" name="profile_pic" id="profile_pic" class="form-control" accept="image/*">
                                     @error('profile_pic')
                                     <div class="text-danger">{{ $message }}</div>
